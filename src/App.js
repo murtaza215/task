ce9375f7de2475a8f8a434964fb1521c7af68c2f
murtaza215 <mohammad.murtaza@ozu.edu.tr>
2023-03-10 16:14:06 +0500

    log dleete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Fragment } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 function App() {
-  console.log("main app");
   return (
     <Fragment>
       <Header />
