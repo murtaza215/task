a9cf4bf46fdd80af2605c66f7e08c2233f7515c5
murtaza215 <mohammad.murtaza@ozu.edu.tr>
2023-03-10 18:39:21 +0500

    Modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Fragment } from 'react';
-
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Modal from './components/UI/Modal';
 function App() {
+  const show=true
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
       <Meals />
+     {show&& <Modal />}
       </main>
     </Fragment>
   );
